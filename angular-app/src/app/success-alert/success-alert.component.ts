11991ffdf484da6886faacc4ee4bc0ffeb0162d1
github username <github email address>
2018-09-07 17:22:49 -0400

    component practice

---
+++ angular-app/src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-success-alert',
+  template: `
+    <p>You are sooo succesful</p>
+  `
+})
+export class SuccessAlertComponent {
+
+}
---
+++ angular-app/src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-warning-alert',
+  template: `
+    <p>You are in danger Will Robinson!!</p>
+  `
+})
+export class WarningAlertComponent {
+
+}
